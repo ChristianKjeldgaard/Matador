bde748bb7d0dd5f29f57e2ec69b9ef655e318cb5
Ali Hassan Tariq <am00ite21@gmail.com>
2021-11-12 10:42:33 +0100

    Addet bank classen

---
+++ Felter/src/Bank.java
@@ -0,0 +1,63 @@
+class Bank {
+
+    private int startingMoney;
+    private int lapMoney;
+    private String currencySymbol;
+    private int wallet;
+
+    public Bank() {
+
+        startingMoney = 0;
+        lapMoney = 0;
+        currencySymbol = new String();
+        wallet = 0;
+    }
+
+    public Bank(String cS, int lM, int sM) {
+
+        startingMoney = sM;
+        lapMoney = lM;
+        currencySymbol = cS;
+
+    }
+
+    public String getCurrencySymbol() {
+        return currencySymbol;
+    }
+
+    public void setCurrencySymbol(String cS) {
+        currencySymbol = cS;
+    }
+
+    public int getStartingMoney() {
+        return startingMoney;
+    }
+
+    public void setStartingMoney(int sM) {
+        startingMoney = sM;
+    }
+
+    public int getLapMoney() {
+        return lapMoney;
+    }
+
+    public void setLapMoney(int lM) {
+        lapMoney = lM;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void withdrawFromWallet(int withdrawAmount) {
+        if (withdrawAmount > wallet) {
+            System.out.println("Player " + /*playerNumber*/ + " went bankrupt!");
+        } else {
+            wallet -= withdrawAmount;
+        }
+        public void depositToWallet ( int depositAmount){
+            wallet += depositAmount;
+            System.out.println("Payday for " + /*getPlayerNumber()*/ +" You have earned " + /*rentCharge*/);
+        }
+    }
+}
