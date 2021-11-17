b2d5a0055c2dec0442d9ab4b22519cf223af2503
Oliver Lundsgaard <oliverlundsgaard@hotmail.com>
2021-11-17 12:43:41 +0100

    ændring i bank og gratisfelt

--- Felter/src/chance/GratisFelt.java
+++ Felter/src/chance/GratisFelt.java
@@ -1,21 +1,37 @@
 package chance;
 
+import com.Felt;
+import com.Spiller;
+
 public class GratisFelt extends Chancekort {
 
-    private Felt feltfarve;
+    private Felt felt;
 
-    public GratisFelt(String iden) {
+    public GratisFelt(String iden, Felt felt) {
         super(iden);
+        this.felt=felt;
     }
 
     @Override
     void brug(Spiller s) {
-        while(!s.getFelt().getFeltfarve().equals(feltfarve)) {
+        while(!s.getFelt().getFeltfarve().equals(felt)) {
             s.ryk(1);
         }
-            if (s.getFelt().getEjer() != null){
-                s.getKonto().påvirk(-Ejendomsfelt.pris(getFelt()));
-        }else s.getFelt().setEjer(s);
 
+        if (s.getFelt().getEjer() != null){
+            s.betal(-felt.getPris());
+            s.getFelt().getEjer().betal(felt.getPris());
+        }else{
+            s.getFelt().setEjer(s);
+        }
+/*
+
+        - GRATIS FELT: Ryk frem til et rødt felt. Hvis ingen ejer den får du den gratis ellers
+        skal du betale leje til ejeren.
+        - GRATIS FELT: Ryk frem til et lyseblåt felt. Hvis ingen ejer den får du den gratis ellers
+        skal du betale leje til ejeren.
+        - GRATIS FELT: Ryk frem til et orange felt. Hvis ingen ejer den får du den gratis ellers
+        skal du betale leje til ejeren.
+        */
     }
 }
--- Felter/src/com/Bank.java
+++ Felter/src/com/Bank.java
@@ -1,9 +1,6 @@
 package com;
 
-import chance.BetalKort;
-import chance.Chancekort;
-import chance.RykKort;
-import chance.RykTilKort;
+import chance.*;
 
 class Bank {
 
@@ -15,10 +12,18 @@ class Bank {
 
     public Bank() {
         chancekort = new Chancekort[16]{
-           new RykTilKort("Ryk til START",/*felt*/),
+           new RykTilKort("Ryk til START",/*Strandpromenaden*/),
            new RykKort("Ryk 5 felter", 5),
            new BetalKort("Du har spist for meget slik. betal M2 til banken", -2);
            new BetalKort("Du har lavet alle dine lektier. Modtag M2 fra banken.", 2);
+           new GratisFelt("Ryk frem til Skaterparken for at lave det perfekte grind. Hvis ingen ejer den får du den gratis\n" +
+                   "    ellers skal du betale leje til ejeren",/*Skaterparken*/);
+           new GratisFelt("GRATIS FELT: Ryk frem til et rødt felt. Hvis ingen ejer den får du den gratis ellers\n" +
+                   "        skal du betale leje til ejeren",/*rødtfelt (spillehallen)*/);
+           new GratisFelt("GRATIS FELT: Ryk frem til et lyseblåt felt. Hvis ingen ejer den får du den gratis ellers\n" +
+                   "        skal du betale leje til ejeren",/*lyseblåtfelt (iskiosken)*/);
+           new GratisFelt("GRATIS FELT: Ryk frem til et orange felt. Hvis ingen ejer den får du den gratis ellers\n" +
+                   "        skal du betale leje til ejeren",/*orangefelt (svømmingpoolen)*/);
         };
         startingMoney = 0;
         lapMoney = 0;
